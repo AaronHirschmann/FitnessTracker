a8f2f9db6a06eb7349fee0b835284b661b0f57f1
DucKimNguyen <s_nguyend24@stud.hwr-berlin.de>
2026-07-13 13:52:49 +0200

    Übungen fertig

--- app/src/main/java/com/example/fitnesstracker/ExerciseFragment.java
+++ app/src/main/java/com/example/fitnesstracker/ExerciseFragment.java
@@ -13,7 +13,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
