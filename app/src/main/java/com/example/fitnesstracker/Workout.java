db10f85e0d571be19991f65c243d44c235376aed
DucKimNguyen <s_nguyend24@stud.hwr-berlin.de>
2026-07-13 18:50:17 +0200

    workout java

---
+++ app/src/main/java/com/example/fitnesstracker/Workout.java
@@ -0,0 +1,13 @@
+package com.example.fitnesstracker;
+
+public class Workout {
+    private String name;
+
+    public Workout(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
