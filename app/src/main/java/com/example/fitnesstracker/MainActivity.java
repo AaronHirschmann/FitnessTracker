c319af9cb476c7c7fe4a26ab56c3259f70679112
Aaron Hirschmann <s_hirschmann24@stud.hwr-berlin.de>
2026-07-13 11:59:01 +0200

    Navigation zwischen Fragments implementiert.

--- app/src/main/java/com/example/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/MainActivity.java
@@ -2,25 +2,42 @@ package com.example.fitnesstracker;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        loadFragment(new HomeFragment());
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+                loadFragment(new HomeFragment());
+            } else if (id == R.id.nav_calendar) {
+                loadFragment(new CalendarFragment());
+            // Einfügen von Übungen und Workouts
+            //} else if (id == R.id.nav_exercises) {
+            } else if (id == R.id.nav_profile) {
+                loadFragment(new ProfileFragment());
+            }
+            return true;
         });
     }
 
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
 
 }
